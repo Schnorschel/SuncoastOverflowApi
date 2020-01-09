c35e6c13a5446e5d03d0ea4deb93499f9ade52f9
Georg Oehl <oehl@pobox.com>
2020-01-08 21:35:53 -0500

    Implemented Search endpoint

---
+++ Controllers/SearchController.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SuncoastOverflowApi.Models;
+
+namespace SuncoastOverflowApi.Controllers
+{
+  [ApiController]
+  [Route("api/[controller]")]
+
+  public class SearchController : ControllerBase
+  {
+    [HttpGet("{searchFor}")]
+    public ActionResult GetSearchResults(string searchFor)
+    {
+      var db = new DatabaseContext();
+      // return Ok(new { searchTerm = $"{searchFor}" });
+      // var searchResults = db.Questions.Include(q => q.Answers) // .OrderByDescending(q => q.LastModifiedDateTime) // , q => q.Answers.AnswerText.ToLower().Contains(searchFor.ToLower())
+      // .Where(q => q.QuestionTitle.ToLower().Contains(searchFor.ToLower()) ||
+      //             q.QuestionText.ToLower().Contains(searchFor.ToLower())
+      // //   || q.Answers.AnswerText.ToLower().Contains(searchFor.ToLower())
+      // );
+
+      // var searchResults = (from q in db.Questions
+      //                      join a in db.Answers on q.Id equals a.QuestionId
+      //                      where q.QuestionTitle.ToLower().Contains(searchFor.ToLower()) ||
+      //                            q.QuestionText.ToLower().Contains(searchFor.ToLower()) ||
+      //                            a.AnswerText.ToLower().Contains(searchFor.ToLower())
+      //                      select q).Distinct().Include(q => q.Answers);
+
+      var searchResults = (from q in db.Questions
+                           join a in db.Answers on q.Id equals a.QuestionId
+                           where q.QuestionTitle.ToLower().Contains(searchFor.ToLower()) ||
+                                 q.QuestionText.ToLower().Contains(searchFor.ToLower()) ||
+                                 a.AnswerText.ToLower().Contains(searchFor.ToLower())
+                           select q).Distinct();
+
+
+      return Ok(searchResults);
+    }
+  }
+}
